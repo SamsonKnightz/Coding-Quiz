bd130e7727793276a958640d79d478463e343202
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-05 22:11:14 -0500

    linked css file, alert set time function

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,11 +1,12 @@
 
-var timerEl = document.getElementById("timer");
+var timeEl = document.getElementById("time");
 
 var secondsLeft = 60;
 
 function setTime(){
     var timerInterval = setInterval(function(){
         secondsLeft--;
+        timeEl.textContent = secondsLeft + "seconds.";
         if(secondsLeft === 0){
             clearInterval(timerInterval);
             return alert("Failure to finish");
