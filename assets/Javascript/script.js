d02c678f3b60d092fe54a90c622306ee32d717da
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-05 21:59:55 -0500

    linked Highscores page

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,10 +1,17 @@
+
 var timerEl = document.getElementById("timer");
 
-var secondsLeft = 10;
+var secondsLeft = 60;
 
 function setTime(){
     var timerInterval = setInterval(function(){
         secondsLeft--;
-      
-    }
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+            return alert("Failure to finish");
+        }
+    }, 1000);
 }
+
+setTime()
+
