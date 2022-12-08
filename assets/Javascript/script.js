644c0292a3d1a4cd87dfa92befa741fd1096c4b5
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-07 22:56:50 -0500

    added simple css

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,13 +1,13 @@
-
+//---linking "time" <div> in html
 var timeEl = document.getElementById("time");
 var secondsLeft = 60;
-
+//---set up display message function to display the timer
 function displayMessage() {
     if (secondsLeft === 1) {
     }
     timeEl.textContent = secondsLeft + " " + "seconds";
 }
-
+//---set a timer on the webpage after 60 seconds, clear timer interval and return an alert.
 function setTime() {
     displayMessage();
     timeEl.textContent = secondsLeft + "seconds";
